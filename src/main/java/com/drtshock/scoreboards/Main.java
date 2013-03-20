c8e65f91619b40e1901230ea4c4aaaed77fd06c6
drtshock <thensler@wisc.edu>
2013-03-19 21:43:49 -0500

    Try a bunch of other stuff. Only to crash client.

--- src/main/java/com/drtshock/scoreboards/Main.java
+++ src/main/java/com/drtshock/scoreboards/Main.java
@@ -1,12 +1,14 @@
 package com.drtshock.scoreboards;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.server.v1_5_R1.Packet;
 import net.minecraft.server.v1_5_R1.Packet206SetScoreboardObjective;
 import net.minecraft.server.v1_5_R1.Packet207SetScoreboardScore;
 import net.minecraft.server.v1_5_R1.Packet208SetScoreboardDisplayObjective;
+import net.minecraft.server.v1_5_R1.Packet209SetScoreboardTeam;
 import net.minecraft.server.v1_5_R1.Scoreboard;
 import net.minecraft.server.v1_5_R1.ScoreboardBaseObjective;
 import net.minecraft.server.v1_5_R1.ScoreboardScore;
@@ -17,9 +19,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_5_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
 	public Main plugin;
 
@@ -27,6 +31,23 @@ public class Main extends JavaPlugin {
 		File file = new File(getDataFolder() + File.separator + "config.yml");
 		if(!file.exists())
 			saveDefaultConfig();
+
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	public void onJoin(PlayerToggleSneakEvent event) {
+		CraftPlayer player = (CraftPlayer) event.getPlayer();
+
+		List<String> roster = new ArrayList<String>();
+		roster.add(player.getName());
+		Packet209SetScoreboardTeam packet = new Packet209SetScoreboardTeam();
+		packet.e = roster;
+		packet.c = " This guy ";
+		packet.b = "JLA";
+		packet.d = " is awesome";
+		packet.f = 0;
+		packet.g = 0;
+		sendPacket(player, packet);
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -39,6 +60,10 @@ public class Main extends JavaPlugin {
 					sender.sendMessage(ChatColor.AQUA + "================");
 					return true;
 				}
+
+				if(!(sender instanceof Player))
+					return true;
+
 				if(args.length == 1) {
 					if(args[0].equalsIgnoreCase("reload")) {
 						reloadConfig();
@@ -48,6 +73,23 @@ public class Main extends JavaPlugin {
 					if(args[0].equalsIgnoreCase("update")) {
 						run();
 						sender.sendMessage(ChatColor.GREEN + "Updated scoreboards for all players!");
+						return true;
+					}
+				}
+				if(args.length == 2) {
+					if(args[0].equalsIgnoreCase("prefix")) {
+						setPrefix((Player) sender, args[1]);
+						sender.sendMessage(ChatColor.GREEN + "Set your prefix to: " + ChatColor.RESET + args[1]);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("suffix")) {
+						setSuffix((Player) sender, args[1]);
+						sender.sendMessage(ChatColor.GREEN + "Set your suffix to: " + ChatColor.RESET + args[1]);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("team")) {
+						setTeam((Player) sender, args[1]);
+						sender.sendMessage(ChatColor.GREEN + "Set your team to: " + ChatColor.RESET + args[1]);
 						return true;
 					}
 				}
@@ -80,6 +122,39 @@ public class Main extends JavaPlugin {
 			}
 		}
 		return;
+	}
+
+	public void setPrefix(Player player, String prefix) {
+		List<String> roster = new ArrayList<String>();
+		roster.add(player.getName());
+		Packet209SetScoreboardTeam packet = new Packet209SetScoreboardTeam();
+		packet.e = roster;
+		packet.c = prefix;
+		packet.f = 0;
+		packet.g = 0;
+		sendPacket(player, packet);
+	}
+
+	public void setSuffix(Player player, String suffix) {
+		List<String> roster = new ArrayList<String>();
+		roster.add(player.getName());
+		Packet209SetScoreboardTeam packet = new Packet209SetScoreboardTeam();
+		packet.e = roster;
+		packet.d = suffix;
+		packet.f = 0;
+		packet.g = 0;
+		sendPacket(player, packet);
+	}
+
+	public void setTeam(Player player, String team) {
+		List<String> roster = new ArrayList<String>();
+		roster.add(player.getName());
+		Packet209SetScoreboardTeam packet = new Packet209SetScoreboardTeam();
+		packet.e = roster;
+		packet.b = team;
+		packet.f = 0;
+		packet.g = 0;
+		sendPacket(player, packet);
 	}
 
 	public void sendPacket(Player player, Packet packet) {
