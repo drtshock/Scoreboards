687b3f73ed6b9aaceedc2805888dd884708387fb
drtshock <thensler@wisc.edu>
2013-03-22 23:50:32 -0500

    Add kill / deaths.

--- src/main/java/com/drtshock/scoreboards/Main.java
+++ src/main/java/com/drtshock/scoreboards/Main.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,7 +15,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin implements Listener {
 
 	HashMap<Player, ScoreboardHandler> handlers = new HashMap<Player, ScoreboardHandler>();
-	
+
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
@@ -25,25 +26,38 @@ public class Main extends JavaPlugin implements Listener {
 		ScoreboardHandler handler = new ScoreboardHandler(this, p);
 		handlers.put(p, handler);
 	}
-	
+
 	@EventHandler
 	public void onPlace(BlockPlaceEvent event) {
 		ScoreboardHandler sbh = handlers.get(event.getPlayer());
 		sbh.increment(ScoreboardHandler.StatType.PlACE_BLOCK);
 		handlers.put(event.getPlayer(), sbh);
 	}
-	
+
 	@EventHandler
 	public void onBreak(BlockBreakEvent event) {
 		ScoreboardHandler sbh = handlers.get(event.getPlayer());
 		sbh.increment(ScoreboardHandler.StatType.BREAK_BLOCK);
 		handlers.put(event.getPlayer(), sbh);
 	}
-	
+
 	@EventHandler
 	public void onQuit(PlayerQuitEvent event) {
 		Player p = event.getPlayer();
 		handlers.remove(p);
+	}
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event) {
+		ScoreboardHandler sbh = handlers.get(event.getEntity());
+		sbh.increment(ScoreboardHandler.StatType.DEATH);
+		handlers.put(event.getEntity(), sbh);
+
+		if(event.getEntity().getKiller() != null) {
+			ScoreboardHandler sbh1 = handlers.get(event.getEntity().getKiller());
+			sbh1.increment(ScoreboardHandler.StatType.KILL);
+			handlers.put(event.getEntity().getKiller(), sbh1);
+		}
 	}
 
 }
--- src/main/java/com/drtshock/scoreboards/ScoreboardHandler.java
+++ src/main/java/com/drtshock/scoreboards/ScoreboardHandler.java
@@ -16,35 +16,55 @@ public class ScoreboardHandler {
 
 	public enum StatType {
 		BREAK_BLOCK,
-		PlACE_BLOCK;
+		PlACE_BLOCK,
+		KILL,
+		DEATH;
 	}
-	
+
 	Scoreboard sb;
 	ScoreboardScore placedBlocksScore;
 	ScoreboardScore breakBlocksScore;
-	
+	ScoreboardScore killScore;
+	ScoreboardScore deathScore;
+
 	Main plugin;
 	EntityPlayer player;
-	
+
 	public ScoreboardHandler(Main m, Player p) {
 		plugin = m;
 		player = ((CraftPlayer) p).getHandle();
 		sb = new Scoreboard();
 		sb.a("Stats", new ScoreboardBaseObjective("Stats"));
-		placedBlocksScore = sb.a(ChatColor.GREEN + "Blocks Placed", sb.b("Stats"));
+		
+		placedBlocksScore = sb.a(ChatColor.GOLD + "Blocks Placed", sb.b("Stats"));
 		placedBlocksScore.c(0);
-		breakBlocksScore = sb.a(ChatColor.RED + "Blocks Broken", sb.b("Stats"));
+		
+		breakBlocksScore = sb.a(ChatColor.GOLD + "Blocks Broken", sb.b("Stats"));
 		breakBlocksScore.c(0);
+		
+		killScore = sb.a(ChatColor.GREEN + "Kills", sb.b("Stats"));
+		killScore.c(0);
+		
+		deathScore = sb.a(ChatColor.DARK_RED + "Deaths", sb.b("Stats"));
+		deathScore.c(0);
+
 		Packet206SetScoreboardObjective scoreboardP = new Packet206SetScoreboardObjective(sb.b("Stats"), 0);
 		Packet208SetScoreboardDisplayObjective displayP = new Packet208SetScoreboardDisplayObjective(1, sb.b("Stats"));
+		
 		Packet207SetScoreboardScore scoreItemBreak = new Packet207SetScoreboardScore(breakBlocksScore, 0);
 		Packet207SetScoreboardScore scoreItemPlace = new Packet207SetScoreboardScore(placedBlocksScore, 0);
+		Packet207SetScoreboardScore scoreKill = new Packet207SetScoreboardScore(killScore, 0);
+		Packet207SetScoreboardScore scoreDeath = new Packet207SetScoreboardScore(deathScore, 0);
+
 		player.playerConnection.sendPacket(scoreboardP);
 		player.playerConnection.sendPacket(displayP);
 		player.playerConnection.sendPacket(scoreItemBreak);
 		player.playerConnection.sendPacket(scoreItemPlace);
+		player.playerConnection.sendPacket(scoreKill);
+		player.playerConnection.sendPacket(scoreDeath);
+
 	}
-	
+
 	public void increment(StatType type) {
 		switch(type) {
 		case BREAK_BLOCK:
@@ -56,6 +76,16 @@ public class ScoreboardHandler {
 			placedBlocksScore.c(placedBlocksScore.c() + 1);
 			Packet207SetScoreboardScore scoreItemPlace = new Packet207SetScoreboardScore(placedBlocksScore, 0);
 			player.playerConnection.sendPacket(scoreItemPlace);
+			break;
+		case KILL:
+			killScore.c(placedBlocksScore.c() + 1);
+			Packet207SetScoreboardScore scoreKill = new Packet207SetScoreboardScore(killScore, 0);
+			player.playerConnection.sendPacket(scoreKill);
+			break;
+		case DEATH:
+			deathScore.c(placedBlocksScore.c() + 1);
+			Packet207SetScoreboardScore scoreDeath = new Packet207SetScoreboardScore(deathScore, 0);
+			player.playerConnection.sendPacket(scoreDeath);
 			break;
 		}
 	}
